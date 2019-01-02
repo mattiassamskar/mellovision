bb51a2688b27abfc59e2d54d4279a86d1762b90c
Mattias Sämskar <mattias.samskar@gmail.com>
2019-01-02 21:23:50 +0100

    Make VotePicker a generic picker

--- src/VotePicker.tsx
+++ src/VotePicker.tsx
@@ -4,21 +4,37 @@ interface State {}
 
 interface Props {
   onChange: (vote: any) => void;
+  values: any[];
+  value: any;
   placeHolder: string;
-  vote: any;
 }
 
 class VotePicker extends React.Component<Props, State> {
   render() {
     const renderOptions = () => {
-      return [1, 2, 3, 4, 5].map(value => (
-        <option value={value}>{value}</option>
+      const placeHolderOption = (
+        <option key="" value="" disabled hidden>
+          {this.props.placeHolder}
+        </option>
+      );
+      const options = this.props.values.map(value => (
+        <option key={value} value={value}>
+          {value}
+        </option>
       ));
+
+      return [placeHolderOption, options];
     };
 
     return (
       <div>
-        <select className="u-full-width">{renderOptions()}</select>
+        <select
+          className="u-full-width"
+          onChange={event => this.props.onChange(event.target.value)}
+          value={this.props.value}
+        >
+          {renderOptions()}
+        </select>
       </div>
     );
   }
