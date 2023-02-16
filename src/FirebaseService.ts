81111a453f1c67fc0bf38c16ebb47a7859f979b5
Mattias Sämskar <mattias.samskar@gmail.com>
2023-02-16 09:17:21 +0100

    Update packages

--- src/FirebaseService.ts
+++ src/FirebaseService.ts
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
 import { v4 as uuidv4 } from "uuid";
 import { Vote } from "./Voting";
 import { UserComment } from "./Chat/Chat";
